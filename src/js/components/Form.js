9eee36ffa99e1c9b00b845de0b7611d66975b1ad
Carol Enache <enache.carol@gmail.com>
2018-07-10 00:00:27 +0100

    create a stateful Form component and render it in the App

---
+++ src/js/components/Form.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import uuidv1 from 'uuid';
+import { addArticle } from '../actions/index';
+
+const mapDispatchToProps = dispatch => {
+    return {
+        addArticle: article => dispatch(addArticle(article))
+    };
+};
+
+class ConnectedForm extends Component{
+    constructor() {
+        super();
+
+        this.state = {
+            title: ''
+        };
+
+        this.handleChage = this.handleChage.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    };
+
+    handleChage(event) {
+        return this.setState({ 
+            [event.target.id]: event.target.value 
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        const { title } = this.state;
+        const id = uuidv1();
+        this.props.addArticle({ title, id });
+        this.setState({title: ''})
+    }
+
+    render() {
+        const { title } = this.state;
+        return (
+            <form>
+                <div className="form-group">
+                    <label htmlFor="title">Title</label>
+                    <input
+                        type='text'
+                        className="form-control"
+                        value={title}
+                        id='title'
+                        onChange={this.handleChage}
+                    />
+                </div>
+                <button type='submit' className="btn btn-success btn-lg">
+                    Save
+                </button>
+            </form>
+        )
+    }
+}
+
+const Form = connect(null, mapDispatchToProps)(ConnectedForm);
+export default Form;
